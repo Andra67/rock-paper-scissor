c12cc45c714831dd154ededccd1e8c161fd88ef7
Julien Andra <julien.andra01@gmail.com>
2022-05-13 16:23:53 +0700

    Add round system, reset, and UI change

--- script.js
+++ script.js
@@ -12,6 +12,23 @@ let computerHandOption = ["cRock", "cPaper", "cScissor"]
 let playerScore = document.getElementById("player-score");
 let computerScore = document.getElementById("computer-score");
 
+let roundNumber = document.getElementById("round-counter");
+let winnerAnnouncer = document.getElementById("announcer");
+
+/*function roundCounter() {
+    if (roundNumber.innerText = 5) {
+        winnerAnnouncer.innerText = "The game is Done";
+    }
+}*/
+
+let resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", resetGame);
+
+function resetGame(){
+    computerScore.innerText = 0;
+    playerScore.innerText = 0;
+    roundNumber.innerText = 0;
+}
 
 function rockHand() {
     document.getElementById("player-hand-image").src = "images/rock.png"
@@ -24,17 +41,31 @@ function rockHand() {
     switch (computerPick) {
         case "cRock": 
             document.getElementById('computer-hand-image').src = "images/rock.png";
+            winnerAnnouncer.innerText = "It's a Draw!";
             break;
         
         case "cPaper" :
             document.getElementById('computer-hand-image').src = "images/paper.png";
-           computerScore.innerHTML ++;
+           computerScore.innerText ++;
+            winnerAnnouncer.innerText = "Computer Win!!!";
             break;
         case "cScissor" :
             document.getElementById('computer-hand-image').src = "images/scissor.png";   
             playerScore.innerText ++;
+            winnerAnnouncer.innerText = "You Win!!!";
             break; 
+
     }
+
+
+    roundNumber.innerText ++;
+    if (roundNumber.innerText == 5) {
+        if (computerScore.innerText > playerScore.innerText) {
+        winnerAnnouncer.innerText = "The game is done, Computer win!";
+    } else {
+        winnerAnnouncer.innerText = "The game is done, You win!";
+    }}
+    //roundCounter()
 
 }
 
@@ -50,16 +81,24 @@ function paperHand() {
         case "cRock": 
             document.getElementById('computer-hand-image').src = "images/rock.png";
             playerScore.innerText ++;
+            winnerAnnouncer.innerText = "You Win!!!";
             break;
         case "cPaper" :
             document.getElementById('computer-hand-image').src = "images/paper.png";
             break;
         case "cScissor" :
             document.getElementById('computer-hand-image').src = "images/scissor.png";
-            computerScore.innerHTML ++;   
+            computerScore.innerText ++;
+            winnerAnnouncer.innerText = "Computer Win!!!";  
             break; 
     }
-    
+    roundNumber.innerText ++;
+    if (roundNumber.innerText == 5) {
+        if (computerScore.innerText > playerScore.innerText) {
+        winnerAnnouncer.innerText = "The game is done, Computer win!";
+    } else {
+        winnerAnnouncer.innerText = "The game is done, You win!";
+    }}
 }
 
 function scissorHand() {
@@ -73,17 +112,25 @@ function scissorHand() {
     switch (computerPick) {
         case "cRock": 
             document.getElementById('computer-hand-image').src = "images/rock.png";
-            computerScore.innerHTML ++;
+            computerScore.innerText ++;
+            winnerAnnouncer.innerText = "Computer Win!!!";
             break;        
         case "cPaper" :
             document.getElementById('computer-hand-image').src = "images/paper.png";
             break;
             playerScore.innerText ++;
+            winnerAnnouncer.innerText = "You Win!!!";
         case "cScissor" :
             document.getElementById('computer-hand-image').src = "images/scissor.png";   
             break; 
     }
+    roundNumber.innerText ++;
+    if (roundNumber.innerText == 5) {
+        if (computerScore.innerText > playerScore.innerText){
+        winnerAnnouncer.innerText = "The game is done, Computer win!";       
+    } else {
+        winnerAnnouncer.innerText = "The game is done, You win!";
+    }}
 }
-
 
 
